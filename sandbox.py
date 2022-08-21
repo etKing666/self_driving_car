79bc64c8d409a891c2786b7ae20158af4d57c8fa
etking <94794807+etKing666@users.noreply.github.com>
2022-08-21 11:08:03 +0300

    v0.9

--- sandbox.py
+++ sandbox.py
@@ -1,40 +1,16 @@
-from abc import ABC, abstractmethod
-from time import sleep
-from sys import exit
+class Car:
+    def __init__(self, status):
+        self.status = status
+
+class User:
+    def change_status(self, Car):
+        Car.status = True
+
+audi = Car(False)
+etkin = User()
+etkin.change_status(audi)
+print(audi.status)
 
 
-def main_menu():
-    print(30 * "=", "SMART CAR INFORMATION SYSTEM (SCIS)", 30 * "=")
-    print("""
-    1. Get information about the car
-    2. Interact with the car
-    3. Exit
-    """)
-    print(97 * "=")
 
-    choice = int(input("Please make your choice [1-3]"))
-    while True:
-        if choice == 1:
-            inf_menu()
-        elif choice == 2:
-            interact_menu()
-        elif choice == 3:
-            print("Thank you for using SCIS!")
-            sleep(2)
-            exit()
-        else:
-            print("You have entered an invalid choice. Please try again.")
-            sleep(2)
-            main_menu()
 
-def inf_menu():
-    print("I am the information menu")
-    exit()
-    pass
-
-def interact_menu():
-    print("I am the interaction menu")
-    exit()
-    pass
-
-main_menu()
